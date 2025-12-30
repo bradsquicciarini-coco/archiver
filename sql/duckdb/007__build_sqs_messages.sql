4c48d56152e24ce47c1c9de2620fc193b1437c17
Brad Squicciarini <80602644+bradsquicciarini-coco@users.noreply.github.com>
2025-12-30 10:35:19 -0800

    wip

--- sql/duckdb/007__build_sqs_messages.sql
+++ sql/duckdb/007__build_sqs_messages.sql
@@ -3,12 +3,53 @@ install spatial
 load spatial
 ;
 
+pragma memory_limit = '14GB'
+;
+pragma temp_directory = '/tmp/duckdb_spill'
+;
 copy (
+with
+routes as (
+    select
+        a.pilot_assignment_id,
+        list(
+            struct_pack(
+                id := r.internal_routing_id,
+                geom_b64 := base64((from_hex(route))),
+                active_start := epoch(r.active_start),
+                active_end := epoch(r.active_end)
+            )
+        ) as routes
+    from './data/20250101_20251130/assignments_and_logs.parquet' a
+    inner join
+        './data/20250101_20251130/routes.parquet' r
+        on r.trip_id = a.external_metadata.trip_id
+        and r.active_start < a.valid_end
+        and a.valid_start < r.active_end
+    group by a.pilot_assignment_id
+),
+
+map_issues as (
+    select
+        pilot_assignment_id,
+        list(
+            struct_pack(
+                id := issueid,
+                reported_location_hexwkb := st_ashexwkb(st_geomfromgeojson(pointgeo)),
+                issue_type := issue_type,
+                created_at := epoch(created_at),
+                notes := notes
+            )
+        ) as map_issues
+    from './data/20250101_20251130/map_issues.parquet'
+    group by pilot_assignment_id
+    )
+
 select
     a.pilot_assignment_id,
     a.device_id,
-    a.valid_start,
-    a.valid_end,
+    epoch(a.valid_start) as valid_start,
+    epoch(a.valid_end) as valid_end,
     a.valid_length_seconds,
     a.n_files,
     a.files as log_files,
@@ -16,9 +57,8 @@ select
     r.routes,
     i.map_issues
 from './data/20250101_20251130/assignments_and_logs.parquet' a
-left join './data/20250101_20251130/matched_assignments_and_routes.parquet' r using (pilot_assignment_id)
-left join './data/20250101_20251130/aggregate_map_issues.parquet' i using (pilot_assignment_id)
-where a.valid_start >= '2025-11-30'::timestamp
-limit 10
-) to './data/20250101_20251130/sqs_messages.jsonl'
+left join routes r using (pilot_assignment_id)
+left join
+    map_issues i using (pilot_assignment_id)
+) to './data/20250101_20251130/sqs_messages.parquet' 
 ;
